5ff2ce3b05823d086c8f97feb43c5a773800bdfa
juan carlos <karlosrt_cobal@hotmail.com>
2019-06-14 19:08:54 -0500

    juan
    caro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './main/inicio/inicio.component';
 import { NormatividadComponent } from './main/normatividad/normatividad.component';
 import { ContratosComponent } from './main/contratos/contratos.component';
+import { ProgramasComponent } from './main/programas/programas.component';
 
 const routes: Routes = [
  {path:'', component:InicioComponent},
  {path:'normatividad', component:NormatividadComponent},
- {path:'contratos' ,component:ContratosComponent}
+ {path:'contratos' ,component:ContratosComponent},
+ {path:'programas',component:ProgramasComponent}
 
 ];
 
